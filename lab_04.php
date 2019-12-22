8a66268e7c264239ec2ef3b0021f79ca10fa7867
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-23 00:08:00 +0200

    Created simple calculator
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- lab_04.php
+++ lab_04.php
@@ -7,5 +7,44 @@
     <title>Calculator</title>
 </head>
 <body>
+    <form action="" method="post">
+        <label for="first_num">First number:</label><br>
+        <input type="number" name="first_num" id="first_num"><br>
+        <label>Sign:</label><br>
+        <input type="radio" name="sign" value="+" id="plus">
+        <label for="plus">+</label><br>
+        <input type="radio" name="sign" value="-" id="minus">
+        <label for="minus">-</label><br>
+        <input type="radio" name="sign" value="*" id="mult">
+        <label for="mult">*</label><br>
+        <input type="radio" name="sign" value="/" id="div">
+        <label for="div">/</label><br>
+        <label for="second_num">Second number:</label><br>
+        <input type="number" name="second_num" id="second_num"><br>
+        <!-- <button>Calculate</button> -->
+        <input type="submit" name="submit"><br>
+
+        <?php
+        if(isset($_POST['submit']) && isset($_POST['sign'])) {
+            echo $_POST['first_num'] . ' ';
+            echo $_POST['sign'] . ' ';
+            echo $_POST['second_num'] . ' = ';
+            switch ($_POST['sign']) {
+                case '+':
+                    echo $_POST['first_num'] + $_POST['second_num'];
+                    break;
+                case '-':
+                    echo $_POST['first_num'] - $_POST['second_num'];
+                    break;
+                case '*':
+                    echo $_POST['first_num'] * $_POST['second_num'];
+                    break;
+                case '/':
+                    echo $_POST['first_num'] / $_POST['second_num'];
+                    break;
+            }
+        }
+        ?>
+    </form>
 </body>
 </html>
